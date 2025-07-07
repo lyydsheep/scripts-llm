555301c8e4494076eb95ad91158c0c1723f5f0ad
lyydsheep <2230561977@qq.com>
2025-07-07 14:54:20 +0800

    feat(chat): add history message recording in SSE handler

--- internal/chat/sse.go
+++ internal/chat/sse.go
@@ -17,12 +17,29 @@ const (
 	Visitor = "visitor"
 )
 
+type Req struct {
+	RoleIdUser      string `json:"role_id_user"`
+	RoleIdAssistant string `json:"role_id_assistant"`
+	Content         string `json:"content"`
+}
+
+// 游客模式/角色扮演
 func SSE(ctx *gin.Context) {
+	var request Req
+	if err := ctx.Bind(&request); err != nil {
+		log.Println("解析请求参数失败: ", "err", err)
+		ctx.JSON(400, gin.H{"error": "参数绑定失败", "status": "error", "message": "参数错误"})
+		return
+	}
+	if request.RoleIdUser == "" {
+		request.RoleIdUser = Visitor
+	}
+
 	url := "https://api.deepseek.com/chat/completions"
 	method := "POST"
 
 	client := &http.Client{}
-	payload, err := getPayload(ctx)
+	payload, err := getPayload(ctx, request)
 	if err != nil {
 		log.Println(err)
 		ctx.JSON(400, gin.H{
@@ -56,6 +73,7 @@ func SSE(ctx *gin.Context) {
 	ctx.Header("Access-Control-Allow-Origin", "*")
 
 	ch := make(chan string)
+	ch2 := make(chan string)
 	// 异步传输消息
 	go func() {
 		for scanner.Scan() {
@@ -65,6 +83,7 @@ func SSE(ctx *gin.Context) {
 				eventData := strings.TrimPrefix(line, "data: ")
 				if eventData == "[DONE]" { // 流结束标记
 					close(ch)
+					close(ch2)
 					break
 				}
 				// 解析 JSON 并提取内容
@@ -74,6 +93,7 @@ func SSE(ctx *gin.Context) {
 						//fmt.Println(event.Choices[0].Delta.Content) // 实时输出内容
 						fmt.Println(eventData)
 						ch <- eventData
+						ch2 <- event.Choices[0].Delta.Content
 					}
 				}
 			}
@@ -82,7 +102,24 @@ func SSE(ctx *gin.Context) {
 			log.Fatal(nil, "读取流失败: ", "err", err)
 		}
 	}()
-
+	go func() {
+		str := ""
+		for msg := range ch2 {
+			str += msg
+		}
+		internal.DB().Model(&internal.Sentence{}).Create(&internal.Sentence{
+			RoleIdUser:      request.RoleIdUser,
+			RoleIdAssistant: request.RoleIdAssistant,
+			Content:         request.Content,
+			Role:            "user",
+		})
+		internal.DB().Model(&internal.Sentence{}).Create(&internal.Sentence{
+			RoleIdUser:      request.RoleIdUser,
+			RoleIdAssistant: request.RoleIdAssistant,
+			Content:         str,
+			Role:            "assistant",
+		})
+	}()
 	// 保持连接并发送消息
 	ctx.Stream(func(w io.Writer) bool {
 		if msg, ok := <-ch; ok {
@@ -94,6 +131,7 @@ func SSE(ctx *gin.Context) {
 	})
 }
 
+// History 获取历史对话
 func History(ctx *gin.Context) {
 	roleIdAssistant, roleIdUser := ctx.Query("role_id_assistant"), ctx.Query("role_id_user")
 	if roleIdUser == "" {
@@ -112,39 +150,49 @@ func History(ctx *gin.Context) {
 	})
 }
 
-func getPayload(ctx *gin.Context) (io.Reader, error) {
-	type Req struct {
-		RoleIdUser      string `json:"role_id_user"`
-		RoleIdAssistant string `json:"role_id_assistant"`
-		Content         string `json:"content"`
-	}
-	var req Req
-	if err := ctx.Bind(&req); err != nil {
-		log.Println("解析请求参数失败: ", "err", err)
-		return nil, err
-	}
-
+// 设置 systemPrompt 和历史对话信息
+func getPayload(ctx *gin.Context, req Req) (io.Reader, error) {
 	payload := NewPayload()
 	// sse
 	payload.Stream = true
+
+	// TODO 设置 prompt
+	var (
+		assistant internal.Role
+		user      internal.Role
+	)
+	// 查询 llm 扮演的角色
+	if err := internal.DB().Model(&internal.Role{}).Where("rid = ?", req.RoleIdAssistant).First(&assistant).Error; err != nil {
+		log.Println("查询人物角色失败: ", "err", err)
+		return nil, err
+	}
+
+	if req.RoleIdUser != Visitor {
+		if err := internal.DB().Model(&internal.Role{}).Where("rid = ?", req.RoleIdUser).First(&user).Error; err != nil {
+			log.Println("查询人物角色失败: ", "err", err)
+			return nil, err
+		}
+	}
+
+	systemPrompt := setSystemPrompt(assistant, user)
 
 	// 添加历史对话
 	sentences := getHistory(req.RoleIdAssistant, req.RoleIdUser)
 	payload.Messages = make([]struct {
 		Content string `json:"content"`
 		Role    string `json:"role"`
-	}, len(sentences))
+	}, len(sentences)+1)
+	payload.Messages[0].Role, payload.Messages[0].Content = "system", systemPrompt
 	for i := range sentences {
-		payload.Messages[i].Role, payload.Messages[i].Content = sentences[i].Role, sentences[i].Content
+		payload.Messages[i+1].Role, payload.Messages[i+1].Content = sentences[i].Role, sentences[i].Content
 	}
+	// 添加当前对话
 	payload.Messages = append(payload.Messages, sentence{
 		Content: req.Content,
 		Role:    "user",
 	})
 
-	// TODO 设置 prompt
-	// 1. 查询人物性格特点
-	// 2. 设置 system prompt
+	fmt.Println(payload.Messages)
 
 	bytes, err := json.Marshal(payload)
 	if err != nil {
@@ -154,15 +202,29 @@ func getPayload(ctx *gin.Context) (io.Reader, error) {
 	return strings.NewReader(string(bytes)), nil
 }
 
+func setSystemPrompt(assistant, user internal.Role) string {
+	var script internal.Script
+	internal.DB().Model(&internal.Script{}).Where("sid = ?", assistant.Sid).First(&script)
+	return fmt.Sprintf("%s\n\n%s\n\n%s\n\n剧本为：\n%s", ChatSystemPrompt, getRoleStr("你", assistant), getRoleStr("用户", user), script.Content)
+}
+
+func getRoleStr(identity string, role internal.Role) string {
+	if role.Rid == "" {
+		return fmt.Sprintf("%s的身份是游客", identity)
+	}
+	return fmt.Sprintf("%s扮演的人物是%s，性格特点为%s，说话习惯是 %s", identity, role.Name, role.Character, role.LanguageHabit)
+}
+
 type sentence struct {
 	Content string `json:"content"`
 	Role    string `json:"role"`
 }
 
+// 获取历史对话
 func getHistory(assistantId, userId string) []sentence {
 	db := internal.DB()
 	var entities []internal.Sentence
-	result := db.Where("assistant_id = ? OR user_id = ?", assistantId, userId).Order("id ASC").Find(&entities)
+	result := db.Where("role_id_assistant = ? AND role_id_user = ?", assistantId, userId).Order("id ASC").Find(&entities)
 	if result.Error != nil {
 		log.Println("查询对话记录失败: ", "err", result.Error)
 		// 只记录，不返回错误
